8abc8aaab83ccd0510e613326da5022d09f59645
c3f2g2 <137452690+c3f2g2@users.noreply.github.com>
2023-12-18 17:10:54 +0100

    Initial commit

--- src/main/java/com/example/bookeshop/models/User.java
+++ src/main/java/com/example/bookeshop/models/User.java
@@ -1,2 +1,26 @@
-package com.example.bookeshop.models;public class User {
+package com.example.bookeshop.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String username;
+    private String emailAddress;
+    private String password;
+
+
 }
