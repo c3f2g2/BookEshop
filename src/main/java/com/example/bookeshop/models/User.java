be444dd23d013db60de1943b5a4af7d7343d1848
c3f2g2 <137452690+c3f2g2@users.noreply.github.com>
2023-12-18 17:10:32 +0100

    Initial commit

---
+++ src/main/java/com/example/bookeshop/models/User.java
@@ -0,0 +1,2 @@
+package com.example.bookeshop.models;public class User {
+}
---
+++ src/test/java/com/example/bookeshop/BookEshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bookeshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookEshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
