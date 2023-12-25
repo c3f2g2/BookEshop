fc535b069213ce5695dde67109e51a594a957489
c3f2g2 <137452690+c3f2g2@users.noreply.github.com>
2023-12-25 19:23:38 +0100

    Basic purchase proceeding implemented

--- src/main/java/com/example/bookeshop/controllers/PurchaseController.java
+++ src/main/java/com/example/bookeshop/controllers/PurchaseController.java
@@ -2,14 +2,23 @@ package com.example.bookeshop.controllers;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @AllArgsConstructor
 public class PurchaseController {
 
-    @PostMapping("/purchase")
-    public void proceedingPurchase(){
+    @PostMapping("/proceed-purchase")
+    public String proceedPurchase() {
 
+        return "purchaseProceeding";
+    }
+
+    @PostMapping("/purchase-confirmation")
+    public String purchaseConfirmation(){
+        return "purchaseConfirmation";
     }
 }
