1f1e455845d9650d27e3f0c93a0cbb8378e3231e
c3f2g2 <137452690+c3f2g2@users.noreply.github.com>
2023-12-27 21:38:28 +0100

    Basic JS for main page added

---
+++ src/main/resources/static/blackBooks.js
@@ -0,0 +1,10 @@
+window.onload = function (){
+    window.alert("Welcome to Black Books, we are having special prices for your favourite books." +
+        " Feel free to check the Featured books section");
+}
+
+window.onload = function() {
+    var featuredBooks = document.getElementById('featured-books');
+    featuredBooks.style.opacity = 1;
+};
+
