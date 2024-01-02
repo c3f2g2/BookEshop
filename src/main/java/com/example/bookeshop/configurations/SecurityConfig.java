a0e661f360ca01f20c503cc32bd1561e04abe628
c3f2g2 <137452690+c3f2g2@users.noreply.github.com>
2024-01-02 11:30:11 +0100

    Implementing basic Spring security

--- src/main/java/com/example/bookeshop/configurations/SecurityConfig.java
+++ src/main/java/com/example/bookeshop/configurations/SecurityConfig.java
@@ -11,33 +11,18 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 @EnableWebSecurity
 public class SecurityConfig {
 
-//    @Bean
-//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//        http
-//                .authorizeHttpRequests(requests -> requests
-//                        .requestMatchers("/vertretungsplan").hasAnyRole("SCHUELER", "LEHRER", "VERWALTUNG")
-//                        .anyRequest().authenticated()
-//                )
-//                .formLogin(form -> form
-//                        .loginPage("/login")
-//                        .permitAll()
-//                )
-//                .logout(logout -> logout
-//                        .permitAll());
-//
-//        return http.build();
-//    }
-
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-//                .requestMatchers(new AntPathRequestMatcher("/")).permitAll()
+//                .requestMatchers(new AntPathRequestMatcher("/", "/basket")).permitAll()
                 .anyRequest().permitAll()
                 .and()
                 .formLogin()
                 .and()
-                .logout();
+                .logout()
+                .and()
+                .csrf().disable();
 
         return http.build();
     }
